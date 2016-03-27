ca43e24aefc14799c0727b3d0d293dcca6fb5597
David Keller <dnkeller@oakland.edu>
2016-03-27 19:54:34 -0400

    added another person

--- src/test/java/com/cse480/alzand/ApiTest.java
+++ src/test/java/com/cse480/alzand/ApiTest.java
@@ -30,43 +30,51 @@ public class ApiTest {
     @Test
     public void test() {
 	response = newUser("dnkeller","shpongle","shpongle","David","Keller","dnkeller@oakland.edu");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = newUser("dnkeller","dingle","dingle","Dolnak","Smeller","dolnakSMELL@yahoo.com");
-	assertEquals(response, "user already exists False");
+	assertEquals("user already exists False", response);
 	response = newUser("nkeller","shpongle","shpongle","David","Keller","dnkeller@oakland.edu");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = auth("dnkeller","shpongle");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	
 	response = newAcq("mZbrg", "Mark", "Zineberg", "female");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = relate("dnkeller", "mZbrg", "Grandson", " My least favorite grandson", "\\model\\s01\\1.pgm", "\\model\\s01\\2.pgm", "\\model\\s01\\3.pgm");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 
 	response = newAcq("lrMD", "Larry", "McDermin", "male");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = relate("dnkeller", "lrMD", "Brother", " My coolest brother. He is so extreme", "\\model\\s02\\1.pgm", "\\model\\s02\\2.pgm", "\\model\\s02\\3.pgm");
+	assertEquals("True", response);
 	
 	response = newAcq("smmcd", "Smiles", "McDermin", "male");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = relate("dnkeller", "smmcd", "Son", " My favorite son. He makes me so happy", "\\model\\s03\\1.pgm", "\\model\\s03\\2.pgm", "\\model\\s03\\3.pgm");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 
 	response = newAcq("gjm", "Grimy", "Jim", "male");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = relate("dnkeller", "gjm", "Neighbor", " He steals my newspapers", "\\model\\s06\\1.pgm", "\\model\\s06\\2.pgm", "\\model\\s06\\3.pgm");
-	assertEquals(response, "True");
+	assertEquals("True", response);
+
 
 	response = newAcq("hj", "Happy", "Johnson", "male");
-	assertEquals(response, "True");
+	assertEquals("True", response);
 	response = relate("dnkeller", "hj", "Caretaker", " He gives me my meds", "\\model\\s07\\1.pgm", "\\model\\s07\\2.pgm", "\\model\\s07\\3.pgm");
-	assertEquals(response, "True");
+	
+
+	response = newAcq("sr", "Shelly", "Ripples", "female");
+	assertEquals("True", response);
+	response = relate("dnkeller", "sr", "Caretaker", " She looks after me", "\\model\\s08\\1.pgm", "\\model\\s08\\2.pgm", "\\model\\s08\\3.pgm");
+	assertEquals("True", response);
 	
 	assertTrue(predict("dnkeller", "\\test\\s01\\4.pgm", "Mark Zineberg", "Grandson", "female", " My least favorite grandson"));
 	assertTrue(predict("dnkeller", "\\test\\s02\\4.pgm", "Larry McDermin", "Brother", "male", " My coolest brother. He is so extreme"));
 	assertTrue(predict("dnkeller", "\\test\\s03\\4.pgm", "Smiles McDermin", "Son", "male", " My favorite son. He makes me so happy"));
 	assertTrue(predict("dnkeller", "\\test\\s06\\4.pgm", "Grimy Jim", "Neighbor", "male", " He steals my newspapers"));
 	assertTrue(predict("dnkeller", "\\test\\s07\\4.pgm", "Happy Johnson", "Caretaker", "male", " He gives me my meds"));
+	assertTrue(predict("dnkeller", "\\test\\s08\\4.pgm", "Shelly Ripples", "Caretaker", "female", " She looks after me"));
     }
 
     private String newUser(String username, String password, String password2, String fname, String lname, String email) {
@@ -180,10 +188,10 @@ public class ApiTest {
 	} catch (Exception e) {fail("predict exception: "+e.toString());}
 	try {
 	jsonParser = new JSONObject(sResponse);
-	assertEquals(jsonParser.getString("ACQUAINTANCE_FNAME")+" "+jsonParser.getString("ACQUAINTANCE_LNAME"), fullname);
-	assertEquals(jsonParser.getString("RELATION"), relation);
-	assertEquals(jsonParser.getString("GENDER"), gender);
-	assertEquals(jsonParser.getString("DESCRIPTION"), description);
+	assertEquals(fullname, jsonParser.getString("ACQUAINTANCE_FNAME")+" "+jsonParser.getString("ACQUAINTANCE_LNAME"));
+	assertEquals(relation, jsonParser.getString("RELATION"));
+	assertEquals(gender, jsonParser.getString("GENDER"));
+	assertEquals(description, jsonParser.getString("DESCRIPTION"));
 	Float distance = Float.parseFloat(jsonParser.getString("DISTANCE"));
 	assertTrue(distance > 0 && distance < cutoff);
 	} catch (Exception e) {fail("JSON exception: "+e.toString());}
